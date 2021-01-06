61ca4c00ac4bf166905f233ba2169a51f043ffdf
brownb202 <brownb202@gmail.com>
2021-01-06 11:18:19 -0700

    added pipes for the bird sprite to dodge

--- src/level.js
+++ src/level.js
@@ -1,13 +1,16 @@
 import { makeSprite, t } from "@replay/core";
-import { Bird } from "./bird";
+import { Bird, birdWidth } from "./bird";
+import { Pipe, pipeWidth, pipeGap } from "./pipe";
 
+const speedX = 2;
 const birdX = 0;
 
 export const Level = makeSprite({
-  init() {
+  init({ device }) {
     return {
       birdY: 10,
       birdGravity: -12,
+      pipes: [newPipe(device)],
     };
   },
 
@@ -16,7 +19,7 @@ export const Level = makeSprite({
       return state;
     }
     const { inputs } = device;
-    let { birdGravity, birdY } = state;
+    let { birdGravity, birdY, pipes } = state;
 
     birdGravity += 0.8;
     birdY -= birdGravity;
@@ -25,9 +28,30 @@ export const Level = makeSprite({
       birdGravity = -12;
     }
 
+    const lastPipe = pipes[pipes.length - 1];
+    if (lastPipe.x < 140) {
+      pipes = [...pipes, newPipe(device)]
+        // Remove the pipes off screen on left
+        .filter(
+          (pipe) =>
+            pipe.x > -(device.size.width + device.size.widthMargin + pipeWidth)
+        );
+    }
+
+    // Move pipes to the left
+    pipes = pipes.map((pipe) => {
+      let passed = pipe.passed;
+      if (!passed && pipe.x < birdX - birdWidth / 2 - pipeWidth / 2) {
+        // Mark pipe as having passed bird's x position
+        passed = true;
+      }
+      return { ...pipe, passed, x: pipe.x - speedX };
+    });
+
     return {
       birdGravity,
       birdY,
+      pipes,
     };
   },
 
@@ -47,6 +71,24 @@ export const Level = makeSprite({
         y: state.birdY,
         rotation: Math.max(-30, state.birdGravity * 3 - 30),
       }),
+      ...state.pipes.map((pipe, index) =>
+        Pipe({
+          id: `pipe-${index}`,
+          pipe,
+          x: pipe.x,
+        })
+      ),
     ];
   },
 });
+
+function newPipe(device) {
+  const height = device.size.height + device.size.heightMargin * 2;
+  const randomY = (height - pipeGap * 2) * (device.random() - 0.5);
+
+  return {
+    x: device.size.width + device.size.widthMargin + 50,
+    gapY: randomY,
+    passed: false,
+  };
+}
