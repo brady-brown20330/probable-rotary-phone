5dd7fa42664d1410d9ecae6886ccf4b473c48957
brownb202 <brownb202@gmail.com>
2021-01-05 22:32:05 -0700

    added sprite to screen and refactored web component

--- web/index.js
+++ web/index.js
@@ -3,6 +3,6 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import { renderCanvas } from "@replay/web";
-import { Game, gameProps, options } from "../src";
+import { Game, gameProps } from "../src";
 
-renderCanvas(Game(gameProps), options);
+renderCanvas(Game(gameProps));
